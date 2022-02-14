44536d7d1dc584d9d2c72a2ab26ad8518190f094
jtagg82 <joseph@jtagg.net>
2022-02-14 11:46:22 +0100

    Made NKRO default

--- keyboards/i9/keymaps/default/keymap.c
+++ keyboards/i9/keymaps/default/keymap.c
@@ -19,22 +19,31 @@ enum my_keycodes {
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _QWERTY,
+    _QW,
+    _CM,
     _FN
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [_QWERTY] = LAYOUT(
-	 KC_ESC,      KC_F1 , KC_F2 , KC_F3 , KC_F4 ,      KC_F5 , KC_F6 , KC_F7 , KC_F8 ,       KC_F9 , KC_F10, KC_F11, KC_F12,   KC_PSCR, ENCO  ,KC_PAUS,                           KC_MUTE,
+    [_QW] = LAYOUT(
+	 KC_ESC,      KC_F1 , KC_F2 , KC_F3 , KC_F4 ,      KC_F5 , KC_F6 , KC_F7 , KC_F8 ,       KC_F9 , KC_F10, KC_F11, KC_F12,   KC_PSCR, ENCO  ,TG(_CM),                          KC_MUTE,
 	 KC_GRV,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,  KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,KC_MINS, KC_EQL,    KC_BSPC    ,    KC_INS,KC_HOME,KC_PGUP,   KC_CALC,KC_PSLS,KC_PAST,KC_PMNS,
 	   KC_TAB  ,  KC_Q ,  KC_W ,  KC_E ,  KC_R ,  KC_T ,  KC_Y ,  KC_U ,  KC_I ,  KC_O ,  KC_P ,KC_LBRC,KC_RBRC,  KC_BSLS  ,    KC_DEL, KC_END,KC_PGDN,    KC_P7 , KC_P8 , KC_P9 ,
 	   MO(_FN)   ,  KC_A ,  KC_S ,  KC_D ,  KC_F ,  KC_G ,  KC_H ,  KC_J ,  KC_K ,  KC_L ,KC_SCLN,KC_QUOT,     KC_ENT      ,                               KC_P4 , KC_P5 , KC_P6 , KC_PPLS,
 	     KC_LSFT     ,  KC_Z ,  KC_X ,  KC_C ,  KC_V ,  KC_B ,  KC_N ,  KC_M ,KC_COMM, KC_DOT,KC_SLSH,       KC_RSFT       ,            KC_UP ,            KC_P1 , KC_P2 , KC_P3 ,
-	 KC_LCTL , KC_LWIN , KC_LALT ,                     KC_SPACE                    , KC_RALT , KC_RWIN , KC_APP  , KC_RCTL ,   KC_LEFT,KC_DOWN,KC_RGHT,        KC_P0     ,KC_PDOT, KC_PENT
+	 KC_LCTL , KC_LWIN , KC_LALT ,                     KC_SPACE                    , KC_RALT , KC_BTN1 , KC_BTN2 , KC_RCTL ,   KC_LEFT,KC_DOWN,KC_RGHT,        KC_P0     ,KC_PDOT, KC_PENT
+    ),
+    [_CM] = LAYOUT(
+	 KC_ESC,      KC_F1 , KC_F2 , KC_F3 , KC_F4 ,      KC_F5 , KC_F6 , KC_F7 , KC_F8 ,       KC_F9 , KC_F10, KC_F11, KC_F12,   KC_PSCR, ENCO  ,_______,                           KC_MUTE,
+	 KC_GRV,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,  KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,KC_MINS, KC_EQL,    KC_BSPC    ,    KC_INS,KC_HOME,KC_PGUP,   KC_CALC,KC_PSLS,KC_PAST,KC_PMNS,
+	   KC_TAB  ,  KC_Q ,  KC_W ,  KC_F ,  KC_P ,  KC_G ,  KC_J ,  KC_L ,  KC_U ,  KC_Y ,KC_SCLN,KC_LBRC,KC_RBRC,  KC_BSLS  ,    KC_DEL, KC_END,KC_PGDN,    KC_P7 , KC_P8 , KC_P9 ,
+	   MO(_FN)   ,  KC_A ,  KC_R ,  KC_S ,  KC_T ,  KC_D ,  KC_H ,  KC_N ,  KC_E ,  KC_I ,  KC_O ,KC_QUOT,     KC_ENT      ,                               KC_P4 , KC_P5 , KC_P6 , KC_PPLS,
+	     KC_LSFT     ,  KC_Z ,  KC_X ,  KC_C ,  KC_V ,  KC_B ,  KC_K ,  KC_M ,KC_COMM, KC_DOT,KC_SLSH,       KC_RSFT       ,            KC_UP ,            KC_P1 , KC_P2 , KC_P3 ,
+	 _______ , _______ , _______ ,                     _______                     , _______ , _______ , _______ , _______ ,   KC_LEFT,KC_DOWN,KC_RGHT,        KC_P0     ,KC_PDOT, KC_PENT
     ),
     [_FN] = LAYOUT(
-        _______,     _______,_______,_______,_______,     _______,_______,_______,_______,      _______,_______,_______,_______,   _______,KC_SCRL,_______,                           _______,
+        A(KC_F4),    _______,_______,_______,_______,     _______,_______,_______,_______,      _______,_______,_______,_______,   _______,KC_SCRL,KC_PAUS,                           _______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______     ,   _______,_______,_______,    KC_NUM,_______,_______,_______,
           _______  ,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,_______, KC_UP ,_______,_______,_______,_______,  _______  ,   _______,_______,_______,   _______,_______,_______,
            _______   ,_______,_______,_______,_______,_______,_______,KC_LEFT,KC_DOWN,KC_RGHT,_______,_______,   _______       ,                              _______,_______,_______,_______,
@@ -128,8 +137,11 @@ bool oled_task_user(void) {
     oled_write_P(PSTR("Layer:    "), false);
 
     switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
+        case _QW:
             oled_write_P(PSTR("QWERTY\n"), false);
+            break;
+        case _CM:
+            oled_write_P(PSTR("COLEMAK\n"), false);
             break;
         case _FN:
             oled_write_P(PSTR("FN\n"), false);
