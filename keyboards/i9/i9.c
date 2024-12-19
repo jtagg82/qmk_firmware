3a1b2a8a1341dc4e0accddf15715619ee8dce2ab
jtagg82 <joseph@jtagg.net>
2024-12-19 12:00:59 +0100

    Cleaned up the code after rebasing. More comments, and removal of deprecated macros in matrix.c

--- keyboards/i9/i9.c
+++ keyboards/i9/i9.c
@@ -3,8 +3,20 @@
 
 #include "i9.h"
 
+/* Encoder behavior is simplified to a mapping of 3 keycodes for its basic functions.
+   This makes it simple to set the desired keycodes to be activated when using the encoder.
+   Use the set_encoder_behavior function to set the keycodes to be used.
+*/
 encoder_behavior_t encoder_behavior = {KC_NO, KC_NO, KC_NO};
 
+void set_encoder_behavior(uint16_t encoder_press, uint16_t encoder_cw, uint16_t encoder_ccw) {
+    encoder_behavior.press = encoder_press;
+    encoder_behavior.cw = encoder_cw;
+    encoder_behavior.ccw = encoder_ccw;
+}
+
+
+/* Simple encoder handler which taps the appropriate encoder_behavior keys. */
 void handle_encoder(encoder_action_t action) {
     if (handle_encoder_user(action)) {
         switch(action) {
@@ -21,16 +33,17 @@ void handle_encoder(encoder_action_t action) {
     }
 }
 
+/* Weak, overridable function so that encoder behavior can be customized within a single function.
+   This bypasses the use of the standard encoder behavior defined here and allows it to be more complex.
+   Returns a true by default, which runs the default behavior. If false is returned in the override,
+   the default behavior will simply be skipped.
+*/
 bool handle_encoder_user(encoder_action_t action) {
     return true;
 }
 
-void set_encoder_behavior(uint16_t encoder_press, uint16_t encoder_cw, uint16_t encoder_ccw) {
-    encoder_behavior.press = encoder_press;
-    encoder_behavior.cw = encoder_cw;
-    encoder_behavior.ccw = encoder_ccw;
-}
 
+/* Override of the standard keyboard record processing.*/
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_user(keycode, record)) return false;
 
@@ -63,10 +76,15 @@ bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+/* Override of the encoder update function which is called when the knob is rotated.
+   Since there is only one encoder, I ignore the index in this function.
+   It simply calls a local handling function to aggregate actions happening with the encoder
+   so that we don't have to spread encoder functionality across multiple locations.
+*/
 bool encoder_update_kb(uint8_t index, bool clockwise) {
+    // Function can be overridden by the user to handle the encoder input
     if (!encoder_update_user(index, clockwise)) return false;
 
-	// Since there is only one encoder, we dispense with the index verification. It keeps things more readable.
 	if (clockwise) {
         handle_encoder(ENC_CW);
     } else {
--- keyboards/i9/matrix.c
+++ keyboards/i9/matrix.c
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "matrix.h"
-#include "gpio.h"
 
 // The 3 pins needed to control the shift registers
 #define SR_INPUT D7
@@ -45,48 +44,48 @@ GND [| 8     9 |] SQh
 
 void matrix_init_custom(void) {
     // Set the pins that control the shift registers as outputs
-    setPinOutput(SR_INPUT);
-    setPinOutput(SR_LATCH);
-    setPinOutput(SR_SHIFT);
+    gpio_set_pin_output(SR_INPUT);
+    gpio_set_pin_output(SR_LATCH);
+    gpio_set_pin_output(SR_SHIFT);
 
     // Set the row pins as inputs that are pulled HIGH when unconnected
-    setPinInputHigh(ROW1);
-    setPinInputHigh(ROW2);
-    setPinInputHigh(ROW3);
-    setPinInputHigh(ROW4);
-    setPinInputHigh(ROW5);
-    setPinInputHigh(ROW6);
+    gpio_set_pin_input_high(ROW1);
+    gpio_set_pin_input_high(ROW2);
+    gpio_set_pin_input_high(ROW3);
+    gpio_set_pin_input_high(ROW4);
+    gpio_set_pin_input_high(ROW5);
+    gpio_set_pin_input_high(ROW6);
 }
 
 inline void shift(void) {
     // toggles both shifting and latching of the shift register
-    writePinHigh(SR_SHIFT);
-    writePinLow(SR_SHIFT);
-    writePinHigh(SR_LATCH);
-    writePinLow(SR_LATCH);
+    gpio_write_pin_high(SR_SHIFT);
+    gpio_write_pin_low(SR_SHIFT);
+    gpio_write_pin_high(SR_LATCH);
+    gpio_write_pin_low(SR_LATCH);
 }
 
 void reset_col(void) {
     // Sets the input of the shift register LOW, which will start a new scan by activating the first column
-    writePinLow(SR_INPUT);
+    gpio_write_pin_low(SR_INPUT);
     shift();
 }
 
 void next_col(void) {
     // When shifting to the next output, we input a HIGH, which means that we are not activating any further columns
-    writePinHigh(SR_INPUT);
+    gpio_write_pin_high(SR_INPUT);
     shift();
 }
 
 
 static uint8_t read_rows(void) {
     // Read all rows and return them as a single byte
-    return ((readPin(ROW1) ? 0 : 1)  |
-	        (readPin(ROW2) ? 0 : 2)  |
-	        (readPin(ROW3) ? 0 : 4)  |
-            (readPin(ROW4) ? 0 : 8)  |
-            (readPin(ROW5) ? 0 : 16) |
-            (readPin(ROW6) ? 0 : 32));
+    return ((gpio_read_pin(ROW1) ? 0 : 1)  |
+	        (gpio_read_pin(ROW2) ? 0 : 2)  |
+	        (gpio_read_pin(ROW3) ? 0 : 4)  |
+            (gpio_read_pin(ROW4) ? 0 : 8)  |
+            (gpio_read_pin(ROW5) ? 0 : 16) |
+            (gpio_read_pin(ROW6) ? 0 : 32));
 }
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
@@ -95,7 +94,7 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
 
     reset_col(); // Activates first column
     for (uint8_t col = 0; col < MATRIX_COLS; col++) {
-        matrix_io_delay();
+        //matrix_io_delay();
         uint8_t rows = read_rows();
 
         // The following for block checks whether anything changed since the last scan
